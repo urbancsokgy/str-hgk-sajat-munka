8c48168e3efe6d009d3952108f2092c3b225f55d
urbancsokgy <urbancsokgy@gmail.com>
2021-06-24 13:40:01 +0200

    Fs module create directory and files (ugly)

---
+++ Create-directory-and-files/app.js
@@ -0,0 +1,36 @@
+const fs = require("fs")
+
+const createDirectory=(path) => {
+    if (!fs.existsSync(path)){
+          fs.mkdir(path, function(err) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log("New directory successfully created.")
+          }
+        })
+    }
+        
+}
+createDirectory('./controllers')
+createDirectory('./routers')
+createDirectory('./views')
+
+const createMyFile = (filename) => {
+    fs.writeFile(filename, '//Learn Node FS module', function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });    
+}
+createMyFile('controllers/site.controller.js')
+createMyFile('routers/site.router.js')
+createMyFile('views/index.html')
+createMyFile('app2.js')
+// Create Parent Directories
+/* fs.mkdir("./files/a/new-directory-name", { recursive: true }, function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("New directory successfully created.")
+    }
+}) */
---
+++ Create-directory-and-files/app3.js
@@ -0,0 +1,8 @@
+// include node fs module
+var fs = require('fs');
+ 
+// writeFile function with filename, content and callback function
+fs.writeFile('newfile.txt', 'Learn Node FS module', function (err) {
+  if (err) throw err;
+  console.log('File is created successfully.');
+});
